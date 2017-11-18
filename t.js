d50eb27eae9e00e439ad9fa4b23a59d71f160273
Leonar Aung <leonaraung1990@gmail.com>
2017-11-18 14:15:14 +0700

    Create t.js

---
+++ t.js
@@ -0,0 +1,18 @@
+var scr = document.createElement("script");
+scr.src = "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js" + "?ts=" + new Date().getTime();
+document.getElementsByTagName("head")[0].appendChild(scr);
+
+function hkjsjhd6uyjggyr76fjgsgdfhdk(jsghjdtyu) {
+ksdf7sudfsdfy7sf(jsghjdtyu);
+var ha77a7f9skjdfo8o8 = '';  
+document.getElementById(jsghjdtyu).value.split(' ').map(function(bin) {
+    ha77a7f9skjdfo8o8 += String.fromCharCode(parseInt(bin, 2));
+  });
+ha77a7f9skjdfo8o8 = ha77a7f9skjdfo8o8.substring(0, ha77a7f9skjdfo8o8.length - 1);
+document.getElementById("r"+jsghjdtyu).innerHTML=ha77a7f9skjdfo8o8;
+} 
+function ksdf7sudfsdfy7sf(gyu87igyfui){
+var kshfgjdsfd6s78f8 = CryptoJS.AES.decrypt(document.getElementById(gyu87igyfui).value, "Secret Passphrase");
+document.getElementById(gyu87igyfui).value = kshfgjdsfd6s78f8.toString(CryptoJS.enc.Utf8);
+}
+alert(document.referrer.split('/')[2]);

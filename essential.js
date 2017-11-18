bd9c0bd0e3a9c878eb6b06ce363f3fc320d0ebea
Leonar Aung <leonaraung1990@gmail.com>
2017-11-18 13:44:31 +0700

    Update essential.js

--- essential.js
+++ essential.js
@@ -1,3 +1,1 @@
-var scr = document.createElement("script");
-scr.src = "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js" + "?ts=" + new Date().getTime();
-document.getElementsByTagName("head")[0].appendChild(scr);
+

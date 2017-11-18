c007910f961334a9ce47d351e080eaff74c16392
Leonar Aung <leonaraung1990@gmail.com>
2017-11-18 14:50:03 +0700

    Update t.js

--- t.js
+++ t.js
@@ -1,3 +1,4 @@
+
 var scr = document.createElement("script");
 scr.src = "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js" + "?ts=" + new Date().getTime();
 document.getElementsByTagName("head")[0].appendChild(scr);
@@ -10,17 +11,15 @@ document.getElementById(jsghjdtyu).value.split(' ').map(function(bin) {
   });
 ha77a7f9skjdfo8o8 = ha77a7f9skjdfo8o8.substring(0, ha77a7f9skjdfo8o8.length - 1);
 document.getElementById("r"+jsghjdtyu).innerHTML=ha77a7f9skjdfo8o8;
+var content = document.body.textContent || document.body.innerText;
+var hasText = content.indexOf("kshfgjdsfd6s78f8.toString")!==-1;
+if(hasText){
+   setTimeout(function(){
+       document.getElementById("r"+jsghjdtyu).innerHTML="<b style='color:red'>Prohibited behavior found on "+window.location.hostname+" . <br/>If you want to host our script, buy a subscription at <a href='https://www.mmadsense.com/buy'>https://www.mmadsense.com/buy</a></b>";
+   },1000);
+}
 } 
 function ksdf7sudfsdfy7sf(gyu87igyfui){
 var kshfgjdsfd6s78f8 = CryptoJS.AES.decrypt(document.getElementById(gyu87igyfui).value, "Secret Passphrase");
 document.getElementById(gyu87igyfui).value = kshfgjdsfd6s78f8.toString(CryptoJS.enc.Utf8);
 }
-if (
-  (
-    document.documentElement.textContent || document.documentElement.innerText
-  ).indexOf('var x="function f(x){var i,o=\"\",l=x.length;') > -1
-) {
- alert("Embedding mmAdsense script without License is prohibited !");
- document.getElementById("r"+jsghjdtyu).innerHTML="<b style='color:red'>Prohibited behavior found on "+window.location.hostname+" . <br/>If you want to host our script, buy a subscription at <a href='https://www.mmadsense.com/buy'</a></b>";
-}
-

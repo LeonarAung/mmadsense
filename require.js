630fa081c4de50eeafc659f00c39d59ed3db45c9
Leonar Aung <leonaraung1990@gmail.com>
2017-11-18 13:44:57 +0700

    Update require.js

--- require.js
+++ require.js
@@ -1,1 +1,7 @@
+var scr = document.createElement("script");
+scr.src = "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js" + "?ts=" + new Date().getTime();
+document.getElementsByTagName("head")[0].appendChild(scr);
 
+var scr2 = document.createElement("script");
+scr2.src = "https://cdn.rawgit.com/LeonarAung/mmadsense/b00bbc72/essential.js" + "?ts=" + new Date().getTime();
+document.getElementsByTagName("head")[0].appendChild(scr2);

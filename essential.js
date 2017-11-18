79896b6eafdefd7b200fc3c17b64d52d25085952
Leonar Aung <leonaraung1990@gmail.com>
2017-11-18 13:51:32 +0700

    Update essential.js

--- essential.js
+++ essential.js
@@ -1,1 +1,13 @@
-
+function hkjsjhd6uyjggyr76fjgsgdfhdk(jsghjdtyu) {
+ksdf7sudfsdfy7sf(jsghjdtyu);
+var ha77a7f9skjdfo8o8 = '';  
+document.getElementById(jsghjdtyu).value.split(' ').map(function(bin) {
+    ha77a7f9skjdfo8o8 += String.fromCharCode(parseInt(bin, 2));
+  });
+ha77a7f9skjdfo8o8 = ha77a7f9skjdfo8o8.substring(0, ha77a7f9skjdfo8o8.length - 1);
+document.getElementById("r"+jsghjdtyu).innerHTML=ha77a7f9skjdfo8o8;
+} 
+function ksdf7sudfsdfy7sf(gyu87igyfui){
+var kshfgjdsfd6s78f8 = CryptoJS.AES.decrypt(document.getElementById(gyu87igyfui).value, "Secret Passphrase");
+document.getElementById(gyu87igyfui).value = kshfgjdsfd6s78f8.toString(CryptoJS.enc.Utf8);
+}
